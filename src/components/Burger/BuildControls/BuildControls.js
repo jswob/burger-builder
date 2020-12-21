8ce59178cf5e407757f917de135f80bf5a220546
jswob <jacswob@gmail.com>
2020-12-21 11:34:36 +0100

    Add addIngredientHandler to BurgerBuilder

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,7 +14,11 @@ const controls = [
 const BuildControls = (props) => (
   <div className={classes.BuildControls}>
     {controls.map((control) => (
-      <BuildControl key={control.label} label={control.label} />
+      <BuildControl
+        key={control.label}
+        label={control.label}
+        added={() => props.ingredientAdded(control.type)}
+      />
     ))}
   </div>
 );
