8a475f1ea64d265397dfac540880d40dd5ef9783
jswob <jacswob@gmail.com>
2020-12-21 11:03:52 +0100

    If there are no selected ingredient show message

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import classes from "./Burger.module.css";
 
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => (
-      <BurgerIngredient key={igKey + i} type={igKey} />
-    ));
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => (
+        <BurgerIngredient key={igKey + i} type={igKey} />
+      ));
+    })
+    .reduce((arr, el) => arr.concat(el), []);
+
+  if (!transformedIngredients.length)
+    transformedIngredients = <p>Please start adding ingredients :)</p>;
 
   return (
     <div className={classes.Burger}>
