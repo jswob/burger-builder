395db9a9e4cf18cd7040b1ee5dba999200ecfbf1
jswob <jacswob@gmail.com>
2020-12-22 11:34:24 +0100

    Create SideDrawer component in basic form

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import classes from "./Layout.module.css";
 
 const Layout = (props) => (
   <Fragment>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Fragment>
 );
